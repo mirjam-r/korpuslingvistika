e55b10c7d1514366c411f075b5688d5f9dd33b98
mirjam-ruutma <mirjam.ruutma@gmail.com>
2017-01-08 01:42:15 +0200

    Add files via upload

---
+++ sonade_arv.sh
@@ -0,0 +1,15 @@
+#Skript loeb kokku, mitu sõna on morfoloogiliselt märgendatud murdetekstis
+#Sisendiks kaustas olevad .xml laiendiga failid
+
+#! /bin/tcsh
+cat *.xml \
+#Iga sõna, mis on eraldatud märgendiga <sone, eraldi reale. Ehk 1 sõna = 1 rida
+| sed 's/\(<sone\)/\n\1/g' \
+#Jäetakse alles ainult sõned, lausemärgendid jm kaovad.
+| grep 'sone' \
+#Eemaldatakse intervjueerija tekst, vahemärgid, märgendaja kommentaarid
+| grep -v 'meta="intervjueerija"' \
+| grep -v 'meta="vahemärk"' \
+| grep -v 'meta="kommentaar"' \
+#Loendatakse ridade arv, ehk mitu sõne on tekstis
+| wc
