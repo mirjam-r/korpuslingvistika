018942bf9a91c0ce84fcf1fa315c40edcc6b1a70
mirjam-ruutma <mirjam.ruutma@gmail.com>
2017-01-08 01:38:11 +0200

    Update ilma_abessiiv_sagedused.sh

--- ilma_abessiiv_sagedused.sh
+++ ilma_abessiiv_sagedused.sh
@@ -1,5 +1,7 @@
 #Skripti väljundiks on fail, kus on ilma+abessiivi esinemise sagedused murdealade kaupa.
 #Sisendiks fail 'ilma_abessiiv.txt' (leiad repositooriumist)
+
+#! /bin/tcsh
 cat ilma_abessiiv.txt \
 #Kustutab ülearuse info ja jätab alles ainult murdeala
 | sed 's/\([^;]*\);.*$/\1/' \
